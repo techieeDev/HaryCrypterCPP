e9f205a6dcf9f244880b3f4842ad26fb9eee548f
pc_techie <pc_techie@DESKTOP-9MU4U3I>
2023-11-12 17:02:59 +0100

    optimized

--- HaryCrypter.h
+++ HaryCrypter.h
@@ -2,13 +2,9 @@
 
 #include<iostream>
 
-using std::string;
-using std::cout;
-using std::endl;
-
 class WHaryCrypter
 {
 public:
-	virtual int Encrypt(string text);
+	virtual int Encrypt(std::string text);
 	void Decrypt(std::string text);
 };
